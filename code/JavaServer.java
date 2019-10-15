f56be3d84d09d6e18845b427e6666a39cf8a2682
Akash Shirale <akashshirale@akashs-mbp.home>
2019-10-15 09:22:57 -0400

    Starter code

--- code/JavaServer.java
+++ code/JavaServer.java
@@ -8,23 +8,23 @@ import org.apache.thrift.transport.TServerTransport;
 import org.apache.thrift.transport.TSSLTransportFactory.TSSLTransportParameters;
 
 // Generated code
-import tutorial.*;
-import shared.*;
+import genjava.*;
+//import shared.*;
 
 import java.util.HashMap;
 
 public class JavaServer {
 
-  public static CalculatorHandler handler;
+  public static OperationHandler handler;
 
-  public static Calculator.Processor processor;
+  public static FileStore.Processor processor;
 
   public static int port;
 
   public static void main(String [] args) {
     try {
-      handler = new CalculatorHandler();
-      processor = new Calculator.Processor(handler);
+      handler = new OperationHandler();
+      processor = new FileStore.Processor(handler);
       port= Integer.valueOf(args[0]);
       Runnable simple = new Runnable() {
         public void run() {
@@ -47,13 +47,13 @@ public class JavaServer {
    
   }
 
-  public static void simple(Calculator.Processor processor) {
+  public static void simple(FileStore.Processor processor) {
     try {
       TServerTransport serverTransport = new TServerSocket(port);
-      TServer server = new TSimpleServer(new Args(serverTransport).processor(processor));
+      //TServer server = new TSimpleServer(new Args(serverTransport).processor(processor));
 
       // Use this for a multithreaded server
-      // TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
+       TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
 
       System.out.println("Starting the simple server...");
       server.serve();
@@ -62,36 +62,5 @@ public class JavaServer {
     }
   }
 
-  public static void secure(Calculator.Processor processor) {
-    try {
-      /*
-       * Use TSSLTransportParameters to setup the required SSL parameters. In this example
-       * we are setting the keystore and the keystore password. Other things like algorithms,
-       * cipher suites, client auth etc can be set. 
-       */
-      TSSLTransportParameters params = new TSSLTransportParameters();
-      // The Keystore contains the private key
-      params.setKeyStore("/home/yaoliu/src_code/local/lib/java/test/.keystore", "thrift", null, null);
-
-      /*
-       * Use any of the TSSLTransportFactory to get a server transport with the appropriate
-       * SSL configuration. You can use the default settings if properties are set in the command line.
-       * Ex: -Djavax.net.ssl.keyStore=.keystore and -Djavax.net.ssl.keyStorePassword=thrift
-       * 
-       * Note: You need not explicitly call open(). The underlying server socket is bound on return
-       * from the factory class. 
-       */
-      TServerTransport serverTransport = TSSLTransportFactory.getServerSocket(port, 0, null, params);
-      TServer server = new TSimpleServer(new Args(serverTransport).processor(processor));
-
-      // Use this for a multi threaded server
-      // TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
-
-      System.out.println("Starting the secure server...");
-      server.serve();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
 }
 
