041ef37de5449cca3c1b7a4c032cd7586aac3b07
Akash Shirale <akashshirale@akashs-mbp.home>
2019-10-18 17:24:22 -0400

    -new suri one

--- client.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash +vx
-LIB_PATH=$"/java/workspace/sourcecode/lib/libthrift-0.10.0.jar:/java/workspace/sourcecode/lib/slf4j-log4j12-1.7.12.jar:/java/workspace/sourcecode/lib/slf4j-api-1.7.12.jar:/java/workspace/sourcecode/lib/log4j-1.2.17.jar"
-#simple/secure ip port
-java -classpath bin/client_classes:$LIB_PATH Client $1 $2
--- server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash +vx
-LIB_PATH=$"/java/workspace/sourcecode/lib/libthrift-0.10.0.jar:/java/workspace/sourcecode/lib/slf4j-log4j12-1.7.12.jar:/java/workspace/sourcecode/lib/slf4j-api-1.7.12.jar:/java/workspace/sourcecode/lib/log4j-1.2.17.jar"
-#port
-java -classpath bin/server_classes:$LIB_PATH Server $1
